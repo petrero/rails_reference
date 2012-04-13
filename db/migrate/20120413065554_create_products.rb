1299504f94a254bd8d597ad1e06acdd3fd757fbc
Petre Diaconescu <petrero@gmail.com>
2012-04-13 10:06:19 +0300

    creating product index action with a get search

---
+++ db/migrate/20120413065554_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.datetime :released_at
+      t.references :category
+
+      t.timestamps
+    end
+    add_index :products, :category_id
+  end
+end
