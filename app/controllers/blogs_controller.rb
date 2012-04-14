2d48f08c4901498e6786c7a115aa9cccdc0e6cbc
Petre Diaconescu <petrero@gmail.com>
2012-04-14 20:48:55 +0300

    making url like pirates.lvh.me:3000(alias localhost:3000) to work and links from the root page without subdomain  to point to their respectve subdomains

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
+    @blog = Blog.find_by_subdomain!(request.subdomain)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 RailsReference::Application.routes.draw do
   resources :blogs
   resources :articles
+  match '', to: 'blogs#show', constraints: {subdomain: /.+/}
   root to: 'blogs#index'
 end
