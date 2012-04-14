7b814926f0051afc7b62560cba9058f00e0bc671
Petre Diaconescu <petrero@gmail.com>
2012-04-14 21:04:14 +0300

    we don't want www to be treated like a subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RailsReference::Application.routes.draw do
   resources :blogs
   resources :articles
-  match '', to: 'blogs#show', constraints: {subdomain: /.+/}
+  match '', to: 'blogs#show', constraints: lambda {|r| r.subdomain.present? && r.subdomain != 'www'}
   root to: 'blogs#index'
 end
