0b429e8f4ec235b2bcbd6fe26c09e9990a064b70
Petre Diaconescu <petrero@gmail.com>
2012-04-15 09:37:51 +0300

    Initial faq

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Faq.create!(
+  question: "Who shot first?",
+  answer: "Han Solo"
+)
+Faq.create!(
+  question: "Is it secret? Is it safe?",
+  answer: "Ask Frodo"
+)
+Faq.create!(
+  question: "When will TextMate 2 be released?",
+  answer: "When Duke Nukem Forever is released... oh wait."
+)
+Faq.create!(
+  question: "What is the greatest game ever invented?",
+  answer: "The game of Go, but it was not invented."
+)
+Faq.create!(
+  question: "What came first, the chicken or the egg?",
+  answer: "I still haven't figured out why the chicken crossed the road."
+)
+Faq.create!(
+  question: "What is the answer to life, the universe, and everything?",
+  answer: "42"
+)
