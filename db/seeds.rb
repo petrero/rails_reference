20f4b85b721f65c90c70e53f0043fd4a709f1e3a
Petre Diaconescu <petrero@gmail.com>
2012-04-16 15:29:41 +0300

    Initial deliver emails site

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Newsletter.create!(subject: "Have you tried turning it off and on again?")
+Newsletter.create!(subject: "Use the Force")
+Newsletter.create!(subject: "1.21 Gigawatts")
+Newsletter.create!(subject: "Anybody want a peanut?")
